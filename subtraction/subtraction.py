1d4e2518372550e28af8ad5c0d7be1bcfdb06cfb
RAGAVI19ITR068 <ragavir.19it@kongu.edu>
2021-04-18 21:36:32 +0530

    comments are added in subtraction.py

--- subtraction/subtraction.py
+++ subtraction/subtraction.py
@@ -1,7 +1,8 @@
+#function for subtraction
 def doSubtraction:
     a=68
     b=10
     return (a-b)
-result=doSubtraction()
+result=doSubtraction() #subtraction function is invoked
 print("Subtracte 10 from 68 is =",result)
 
