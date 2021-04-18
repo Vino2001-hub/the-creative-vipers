75e6c290a08fc03e21f23aad206d28939c9df86e
SAKTHI KARTHI LINGAM <you@example.com>
2021-04-18 21:37:55 +0530

    comment is added in multiplication.py

--- multiplication/multiplication.py
+++ multiplication/multiplication.py
@@ -1,5 +1,5 @@
 def domultiplication:
     a=68
     b=10
-    print("Multiplication of 68*10=",a*b)
+    print("Multiplication of 68*10=",a*b)#multiplication function is invoked
 domultiplication()
